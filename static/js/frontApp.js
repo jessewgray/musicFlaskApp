594d850bc2cc86fe964087b46c6e371cca4812fd
Jesse Gray <jwgray2@gmail.com>
2019-03-16 23:54:49 -0400

    new updates

--- static/js/frontApp.js
+++ static/js/frontApp.js
@@ -1,79 +1,27 @@
 window.addEventListener("load", function(){
-   
-	//console.log("working")
-
+	var thebutton = window.document.getElementById("clicker");
+	
 
 	var baseURL = window.location.href
 	var api = "api"
 
-	// var country = "England"
-	// var artist = "blur"
-	// var newAPI = "api/" + country + "/" + artist
-	// var countryAPI = "api/" + country
 
-
-
-
-//###################################
 	const newUniqueCountries = []
 	 d3.json(baseURL + api).then(function(data) {
-  		//console.log(data);
 
-		//Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(data[key]['year'] + ', ' + data[key]['artist'] + ', ' + data[key]['country'] + ', ' +data[key]['title'] + ', ' + data[key]['duration'] ));
 		uniqueCountries = data.map(x => x.country.trim()).filter((v, i, a) => a.indexOf(v) === i); 
 		uniqueCountries.sort()
 		
-		//console.log(uniqueCountries)
 
 		for (i = 0; i < uniqueCountries.length; i++){
 			newUniqueCountries.push(uniqueCountries[i])
 		}
-		//var uniqueArtists = data.map(x => x.artist.trim()).filter((v, i, a) => a.indexOf(v) === i); 
-		//console.log(uniqueArtists.sort())
-
-		//uniqueCountries.forEach(country => console.log(country));
-		//uniqueArtists.forEach(artist => d3.select("#theArtists").append('option').html(artist));
 
 	});
-	//console.log(newUniqueCountries)
- 
-	
-
-
-
-	// d3.select("#theCountries").on("change", function(){
-	// 	// var aCountry = document.getElementById("theCountries");
-	// 	// console.log(aCountry.options[aCountry.selectedIndex].value)
-	// 	var aCountry = document.getElementById("theCountries");
-	// 	var aArtist = document.getElementById("theArtists");	
-	// 	var country = aCountry.options[aCountry.selectedIndex].value;
-	// 	var artist = aArtist.options[aArtist.selectedIndex].value;
-	// 	var newAPI = "api/" + country + "/" + artist;
-	// 	d3.select(".section-content-ul").selectAll("li").remove();
-	// 	d3.json(baseURL + newAPI).then(function(data) {
-				
-	// 			Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(data[key]['year'] + ', ' + data[key]['artist'] + ', ' + data[key]['country'] + ', ' +data[key]['title'] + ', ' + data[key]['duration'] ));
-
-	// 		});
-	// });
-	// d3.select("#theArtists").on("change", function(){
-	// 	var aCountry = document.getElementById("theCountries");
-	// 	var aArtist = document.getElementById("theArtists");
-	// 	var country = aCountry.options[aCountry.selectedIndex].value;
-	// 	var artist = aArtist.options[aArtist.selectedIndex].value;
-	// 	var newAPI = "api/" + country + "/" + artist;
-	// 	d3.select(".section-content-ul").selectAll("li").remove();
-	// 	d3.json(baseURL + newAPI).then(function(data) {
-				
-	// 			Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(data[key]['year'] + ', ' + data[key]['artist'] + ', ' + data[key]['country'] + ', ' +data[key]['title'] + ', ' + data[key]['duration'] ));
-
-	// 		});
-	// });
 
 
 
 d3.select("#theArtists").on("change", function(){
-	//console.log('Jesse')
 	var labelArtistSong = []
 	var ySongDiration = []
 	var xSongYear = []
@@ -81,24 +29,19 @@ d3.select("#theArtists").on("change", function(){
 
 	var aArtist = document.getElementById("theArtists");
 	var artist = aArtist.options[aArtist.selectedIndex].value;
-	//console.log(artist);
+	
 	var artistBaseURL = "api/filter"
 	d3.json(baseURL + api + "/filter/" + artist).then(function(data) {
-		//console.log(data)
+		
 		d3.select(".section-content-ul").selectAll("li").remove();
-		Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(data[key]['year'] + ', ' + data[key]['artist'] + ', ' + data[key]['country'] + ', ' +data[key]['title'] + ', ' + data[key]['duration'] ));
+		Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(`<div class="row"><div class="col-md-4 returnYear"><h2>${data[key]['year']}</h2></div> <div class="col-md-8 returnArtistSong"><h2>${data[key]['artist']}</h2><h4>${data[key]['title']}</h4></div></row>`));
 		
 
 		data.forEach(x => labelArtistSong.push(x.title));
 		data.forEach(y => ySongDiration.push(y.duration));
 		data.forEach(z => xSongYear.push(z.year))
-		// console.log(labelArtistSong);
-		// console.log(ySongDiration);
-		// console.log(xSongYear);
-		console.log('remove plot two')
+		
 		d3.select("#plotsTwo").selectAll("div").remove();
-
-		//
 
 		var trace1 = {
   			x: xSongYear,
@@ -119,28 +62,9 @@ d3.select("#theArtists").on("change", function(){
 		
 
 		Plotly.newPlot('plotsTwo', data, layout);
-		//
 
 	});
 });
-
-
-//#####################################
-
-
-	// d3.json(baseURL + api).then(function(data) {
- //  		console.log(data);
-	// 	Object.keys(data).forEach(key => d3.select("#theCountries").append('option').html(data[key]['country']));
-	// 	Object.keys(data).forEach(key => d3.select("#theArtists").append('option').html(data[key]['artist']));
-
-	// d3.select("#theCountries")
-	// .append("option")
-	// .html("select country");
-
-	// d3.select("#theArtists")
-	// .append("option")
-	// .html("select artist");
-	// });
 
 
 
@@ -160,30 +84,24 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 
 
-//console.log(countries)
-//L.geoJSON(countries).addTo(myMap)
-
-
 var geoJsonLayer = L.geoJson(countries, {
 
   // Executes on each feature in the dataset
   onEachFeature: function (featureData, featureLayer) {
-  	//console.log(featureData)
+
   	if (featureData.properties.name == "Canada"){
-  		console.log(featureData);
-  		var myStyle = {
-    	"color": "red",
+  	var myStyle = {
+    	"color": "rgb(224,49,37)",
     	"weight": 5,
     	"opacity": 0.65
-};
+	};
 
-L.geoJSON(countries[28], {
+L.geoJSON([countries[28], countries[8], countries[13], countries[44], countries[53], countries[56], countries[42], countries[76], countries[79],countries[81], countries[82], countries[121], countries[123], countries[143], countries[153],countries[170], countries[58]], {
     style: myStyle
 }).addTo(myMap);
   	}
 
 
-  	//console.log(featureLayer)
     // featureData contains the actual feature object
     // featureLayer contains the indivual layer instance
     featureLayer.on('click', function () {
@@ -193,21 +111,16 @@ L.geoJSON(countries[28], {
 
   		d3.select("#theArtists").append('option').html("Filter by Artist")
       // Fires on click of single feature
-      //console.log('Clicked feature layer ID: ' + featureData.id + ", " + featureData.properties.name);
       var baseURL = window.location.href;
       var countryAPI = "api/" + featureData.properties.name;
       d3.json(baseURL + countryAPI).then(function(data) {
 				
-      		console.log(data)
       		if (data.length === 0){
-      			//console.log(data.length)
-      			d3.select('.section-content-ul').append('li').html("No hits from " + featureData.properties.name + ", click a <span class='red'>red</span> country")
+      			d3.select('.section-content-ul').append('li').html(`<p class="noHitText">There are 0 hits from <span>${featureData.properties.name}</span>. Try clicking on a <span class='red'>red</span> country.</p>`)
       		}else{
-      		Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(data[key]['year'] + ', ' + data[key]['artist'] + ', ' + data[key]['country'] + ', ' +data[key]['title'] + ', ' + data[key]['duration'] ));
-
-			var uniqueArtists = data.map(x => x.artist.trim()).filter((v, i, a) => a.indexOf(v) === i); 
-			uniqueArtists.sort()
-			//Object.keys(data).forEach(key => d3.select("#theCountries").append('option').html(data[key]['country']));
+      		Object.keys(data).forEach(key => d3.select('.section-content-ul').append('li').html(`<div class="row"><div class="col-md-4 returnYear"><h2>${data[key]['year']}</h2></div> <div class="col-md-8 returnArtistSong"><h2>${data[key]['artist']}</h2><h4>${data[key]['title']}</h4></div></row>`))
+      		var uniqueArtists = data.map(x => x.artist.trim()).filter((v, i, a) => a.indexOf(v) === i); 
+			uniqueArtists.sort();
 			uniqueArtists.forEach(artist => d3.select("#theArtists").append('option').html(artist));
 			}
 		});
@@ -218,7 +131,6 @@ L.geoJSON(countries[28], {
 
       var countryHitsAPI = "api/data/filter/" + featureData.properties.name;
       d3.json(baseURL + countryHitsAPI).then(function(data){
-      	console.log(data)
       	for (i = 0; i<data.length; i++){
       	ar.push(data[i].artist)
       	nm.push(parseInt(data[i].numberOfHits))
@@ -237,16 +149,12 @@ L.geoJSON(countries[28], {
     		marker: {
     		color: 'rgb(224,49,37)'
   			},
-    	// 	x: [1999, 2000, 2001, 2002],
-  			// y: [10, 15, 13, 17],
     		type: 'bar'
   		}
 		];
 		var layout = {
   			autosize: false,
-  			// height: 500,
   			title: 'Triple J\s Hottest 100 Hits',
-  			xaxis: {title: 'Artist'},
   			yaxis: {title: '# of Hits'}
   		}
 		
@@ -254,7 +162,6 @@ L.geoJSON(countries[28], {
 	}
 setTimeout(function(){ timeIt(); }, 1500);
       	//end chart countries
-      	console.log('still working')
     });
   }
 
@@ -268,9 +175,18 @@ var layout = {
   		}
 
 
+d3.select(thebutton).on("click", function(){
+		console.log('clicking is working');
 
+		var a = window.document.getElementsByTagName('video');
+		console.log(a);
+		//a.play();
+	});
 
+console.log("loaded");
 });
+
+
 
 
 
